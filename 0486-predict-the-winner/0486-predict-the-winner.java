460e85a9b08976bdedaacac698eb63ccbe74f0c9
VishantNimesh <151125339+VishantNimesh@users.noreply.github.com>
2026-08-01 14:17:13 +0530

    Time: 65 ms (28.69%), Space: 42.8 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        return solve(nums, 0, nums.length - 1) >= 0;
+    }
+    private int solve(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+        int chooseLeft = nums[left] - solve(nums, left + 1, right);
+        int chooseRight = nums[right] - solve(nums, left, right - 1);
+
+        return Math.max(chooseLeft, chooseRight);
+    }
+}
